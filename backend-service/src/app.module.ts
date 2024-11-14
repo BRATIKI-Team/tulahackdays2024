086c1c069e1745500736c52f4bacf4c49bd56aa1
andrey_fesunov <fsnv04@gmail.com>
2024-11-14 10:10:32 +0300

    feat: add telegraf lib

--- backend-service/src/app.module.ts
+++ backend-service/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from '@users/users.module';
+import { TelegrafModule } from 'nestjs-telegraf';
 import { AppController } from './app.controller';
 import { DoctorsModule } from './doctor/doctors.module';
 
 @Module({
   imports: [
     UsersModule,
-    DoctorsModule
+    DoctorsModule,
+    TelegrafModule.forRoot({
+      token: '7868217310:AAGi-c7r4SRkL20eh_m4cpNjHaJjQNED1Ws',
+    }),
   ],
   controllers: [AppController],
   providers: [],
